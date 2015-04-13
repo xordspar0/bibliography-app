b04ce4ea4e18f2d404c669f02ae50e828adf7f01
Jordan Christiansen <xordspar0@gmail.com>
2015-04-13 10:38:01 -0500

    Added the web citation page

---
+++ add_citation_web.php
@@ -0,0 +1,32 @@
+<!Doctype html>
+<html>
+
+	<head>
+		<title>Add a web citation</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<nav>
+			<a href="javascript:history.back()">Back</a>
+		</nav>
+
+		<form action="get_web_source.php" method="post">
+			<table>
+				<tr>
+					<td>By URL:</td>
+					<td><input type="text" name="url" required /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Submit" /></td>
+				</tr>
+			</table>
+		</form>
+
+		<a href="add_citation_manual.php">Manual</a>
+
+	</body>
+
+</html>
