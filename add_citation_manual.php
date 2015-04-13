65b12434cdec968cbe5e471607a2b66adb996cb9
Jordan Christiansen <xordspar0@gmail.com>
2015-04-13 11:33:30 -0500

    Added manual citation page

---
+++ add_citation_manual.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+<html>
+
+	<head>
+		<title>Add a citation manually</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<nav>
+			<a href="javascript:history.back()">Back</a>
+		</nav>
+
+		<form action="get_web_source.php" method="post">
+			<table>
+				<tr>
+					<td>Title:</td>
+					<td><input type="text" name="title" required /></td>
+				</tr>
+				<tr>
+					<td>Author:</td>
+					<td><input type="text" name="author" required /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Submit" /></td>
+				</tr>
+			</table>
+		</form>
+
+	</body>
+
+</html>
