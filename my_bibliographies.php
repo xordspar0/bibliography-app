ff11e2f477997a3841792233104bce525620c1da
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 13:11:49 -0500

    Update my_bibliographies.php

--- my_bibliographies.php
+++ my_bibliographies.php
@@ -2,15 +2,16 @@
   <html>
     <head>
       <title>My Bibliographies</title>
+      <h1>Manage Your Bibliographies</h1>
       <div align="right">
         <a href="bibliography_management.php">Back</a>
       	<a href="login.php">Logout</a>
       </div>
     </head>
     <body>
-      //Add a list of Bibliographies based on the query 
-      //"SELECT bibliographies.name 
-      // FROM bibliographies, users 
-      // WHERE users.userID = (ID of the current user) AND users.userID = bibliographies.userID"
+      <!--Add a list of Bibliographies based on the query 
+      "SELECT bibliographies.name 
+       FROM bibliographies, users 
+       WHERE users.userID = (ID of the current user) AND users.userID = bibliographies.userID"-->
     </body>
   </html>
