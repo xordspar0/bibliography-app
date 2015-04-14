2740f5aea19938ec85cab02e28347cc207321434
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 12:34:54 -0500

    Create bibliography_management.php
    To do:
    -Give the user logout functionality from this point onward
    -Make the "My Bibliographies link also display the numbner of bibliographies for this user in the form of My Bibliographis(n), n being the count of bibliographies.

---
+++ bibliography_management.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Manage your bibliographies</title>
+		</head>
+		<body>
+			<div align="right">
+				<a href="Login.php">Logout</a>
+			</div>
+			<nav>
+				<a href="my_bibliographies.php">My Bibliographies</a>
+				<br><br>
+				<a href="create_new_bibliography.php">Create a new Bibliography</a>
+		</body>
+	</html>
