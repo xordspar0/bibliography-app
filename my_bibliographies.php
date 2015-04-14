d34b7e8bb0db830fa8386636e75c7d36e406dd86
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 12:45:26 -0500

    Create my_bibliographies.php
    TO do:
    -Add query
    -Allow user to click and "Edit" or a "Delete" button next to each row from the query displayed
    -Allow the user to click the query name and be brought to "view_contents.php"

---
+++ my_bibliographies.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>My Bibliographies</title>
+      <div align="right">
+        <a href="bibliography_management.php">Back</a>
+      	<a href="login.php">Logout</a>
+      </div>
+    </head>
+    <body>
+      //Add a list of Bibliographies based on the query 
+      //"SELECT bibliographies.name 
+      // FROM bibliographies, users 
+      // WHERE users.userID = (ID of the current user) AND users.userID = bibliographies.userID"
+    </body>
+  </html>
