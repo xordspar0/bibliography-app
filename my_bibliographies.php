589f435debb9d71ba382911887bc4a2b2d6aa311
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 13:19:56 -0500

    Update my_bibliographies.php

--- my_bibliographies.php
+++ my_bibliographies.php
@@ -2,11 +2,11 @@
   <html>
     <head>
       <title>My Bibliographies</title>
-      <h1>My Bibiographies</h1>
       <div align="right">
         <a href="bibliography_management.php">Back</a>
       	<a href="login.php">Logout</a>
       </div>
+      <h1>My Bibiographies</h1>
     </head>
     <body>
       <!--Add a list of Bibliographies based on the query 
