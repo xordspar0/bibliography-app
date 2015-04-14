e8431400cf07e05acfa8873d529c6d7418fb4c62
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 13:07:18 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 		</head>
 		<body>
 			
-			<form method="post" action="Bibliography_Management.php">
+			<form method="post" action="bibliography_banagement.php">
 			Name: <br><input type="text" name="name"><br>
 			Password: <br><input type="text" name="password"><br><br>
 			<input id="button" type="submit" name="submit" value="Log-In">
