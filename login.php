979546f539415c71e3f0a07c67191f738d940488
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 12:54:11 -0500

    Update login.php
    TO do:
    -create user verification using a query to the users table

--- login.php
+++ login.php
@@ -1,14 +1,17 @@
 <!DOCTYPE HTML>
-<html>
-<body>
-	
-	<form method="post" action="Bibliography_Management.php">
-	Name: <br><input type="text" name="name"><br>
-	Password: <br><input type="text" name="password"><br><br>
-	<input id="button" type="submit" name="submit" value="Log-In">
-	</form>
-
-	<a href="Register.php">Register</a>
-
-</body>
-</html>
+	<html>
+		<head>
+			<title>Login</title>
+		</head>
+		<body>
+			
+			<form method="post" action="Bibliography_Management.php">
+			Name: <br><input type="text" name="name"><br>
+			Password: <br><input type="text" name="password"><br><br>
+			<input id="button" type="submit" name="submit" value="Log-In">
+			</form><br>
+		
+			<a href="Register.php">Register</a>
+		
+		</body>
+	</html>
