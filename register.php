c6368ff61dcc0f2ef293f55633ba283ae6218003
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 12:51:33 -0500

    Update register.php
    To do: 
    -Add input validation
    -allow for required fields
    -allow the new user to be added to table users on click of Register button

--- register.php
+++ register.php
@@ -1,28 +1,32 @@
 <!DOCTYPE HTML>
-<html>
-<body>
-  <div align="right">
-  	<a href="Login.php">Back</a>
-  </div>
-
-
-
-  //<p><span class="error">* required field.</span></p>
-  <form action="Login.php" method="post">
-  User-ID: <br><input type="text" name="userID">
-	
-  <br>
-  First: <br><input type="text" name="firstName"><br>
-  Last: <br><input type="text" name="lastName"><br>
-  Password: <br><input type="text" name="password">
-	
-  <br>
-  Re-Enter Password: <br><input type="text" name="rePassword">
-
-  <br>
-  <input id="button" type="submit" name="register" value="Register">
-
-
-
-</html>
-</body>
+	<html>
+		<head>
+			<title>Register a new user</title>
+			<div align="right">
+		  	<a href="Login.php">Back</a>
+		  </div>
+		</head>
+		<body>
+		  
+		
+		
+		
+		  //<p><span class="error">* required field.</span></p>
+		  <form action="Login.php" method="post">
+		  User-ID: <br><input type="text" name="userID">
+			
+		  <br>
+		  First: <br><input type="text" name="firstName"><br>
+		  Last: <br><input type="text" name="lastName"><br>
+		  Password: <br><input type="text" name="password">
+			
+		  <br>
+		  Re-Enter Password: <br><input type="text" name="rePassword">
+		
+		  <br>
+		  <input id="button" type="submit" name="register" value="Register">
+		
+		
+		
+		</body>
+	</hrml>
