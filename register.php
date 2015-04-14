dd1e8cf1aa8ddb120f7ce28e1fc31bfb44a3f8cc
JoshMindermann <joshmindermann@yahoo.com>
2015-04-14 12:17:24 -0500

    Create register.php
    To do:
    -finish required fields functionality.
    -Allow the new user to be added to table users on click of the Register button
    -Allow for validation of inputs

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+  <div align="right">
+  	<a href="Login.php">Back</a>
+  </div>
+
+
+
+  //<p><span class="error">* required field.</span></p>
+  <form action="Login.php" method="post">
+  User-ID: <br><input type="text" name="userID">
+	
+  <br>
+  First: <br><input type="text" name="firstName"><br>
+  Last: <br><input type="text" name="lastName"><br>
+  Password: <br><input type="text" name="password">
+	
+  <br>
+  Re-Enter Password: <br><input type="text" name="rePassword">
+
+  <br>
+  <input id="button" type="submit" name="register" value="Register">
+
+
+
+</html>
+</body>
