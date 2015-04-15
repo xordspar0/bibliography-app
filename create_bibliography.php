d9548865d48610f31f905408e883a7a19a4df9e4
Jordan Christiansen <xordspar0@gmail.com>
2015-04-15 12:23:06 -0500

    Added create bibliography page

---
+++ create_bibliography.php
@@ -0,0 +1,32 @@
+<!Doctype html>
+<html>
+
+	<head>
+		<title>Create a Bibliography</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<nav>
+			<div align="right">
+				<a href="javascript:history.back()">Back</a>
+			</div>
+		</nav>
+
+		<form action="add_citation_web.php" method="post">
+			<table>
+				<tr>
+					<td>Name:</td>
+					<td><input type="text" name="bibName" required /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Next" /></td>
+				</tr>
+			</table>
+		</form>
+
+	</body>
+
+</html>
