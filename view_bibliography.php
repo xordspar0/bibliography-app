35f924150fa5446cc4104a1a9ae80a158997b167
Jordan Christiansen <xordspar0@gmail.com>
2015-04-15 12:28:43 -0500

    Added print page

---
+++ view_bibliography.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+
+	<head>
+		<title>View Bibliography</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<nav>
+			<div align="right">
+				<a href="javascript:history.back()">Back</a>
+			</div>
+		</nav>
+
+		<ul class="actions">
+			<a href="edit_bibliography.php">Edit Contents</a>
+			<a href="#">Print</a>
+		</ul>
+
+	</body>
+
+</html>
