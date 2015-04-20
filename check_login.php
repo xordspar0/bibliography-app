6a039511d41f52b3b93fdd1362fae519f2f6eab7
JoshMindermann <joshmindermann@yahoo.com>
2015-04-20 12:06:33 -0500

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,37 +1,36 @@
 <!DOCTYPEHTML>
-        <html>
-        $uName=$pWord=$errorMsg="";
-                <body>
-                        <?php
-                                session_start();
-                                $uName=$_POST["name"];
-                                $pWord=$_POST["pWord"];
-                                
-                                // The connection string is loooooooong. It's easiest to copy/paste this line. Remember to replace 'username' and 'password'!
-                                $conn = oci_connect('username', 'password', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-                                
-                                //put your query in here
-                                $query = 'SELECT users.userID, users.password FROM users WHERE users.userID='$uName' AND users.password='$pWord'';
-                                
-                                $stid = oci_parse($conn,$query);
-                                oci_execute($stid,OCI_DEFAULT);
-                                
-                                
-                                $check=0;
-                                while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
-                                {
-                                   foreach ($row as $item) 
-                                   {
-                                      $check=$check+1;
-                                   }
-                                }
-                                oci_free_statement($stid);
-                                oci_close($conn); 
-                                
-                                if(check==1)
-                                {
-                                        
-                                }
-                        ?>
-                </body>
-        </html>
+
+        <?php
+                $uName=$pWord=$errorMsg="";
+                
+                session_start();
+                $uName=$_POST["name"];
+                $pWord=$_POST["pWord"];
+                
+                // The connection string is loooooooong. It's easiest to copy/paste this line. Remember to replace 'username' and 'password'!
+                $conn = oci_connect('username', 'password', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+                
+                //put your query in here
+                $query = 'SELECT users.userID, users.password FROM users WHERE users.userID='$uName' AND users.password='$pWord'';
+                
+                $stid = oci_parse($conn,$query);
+                oci_execute($stid,OCI_DEFAULT);
+                
+                
+                $check=0;
+                while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+                {
+                   foreach ($row as $item) 
+                   {
+                      $check=$check+1;
+                   }
+                }
+                oci_free_statement($stid);
+                oci_close($conn); 
+                
+                if(check==1)
+                {
+                        $_SESSION['name']=$uName;
+                }
+        ?>
+    
