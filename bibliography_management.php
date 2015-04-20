f363c39cf2ef43d2bf78fd5fc9dbc490604234ec
joshmindermann <joshmindermann@yahoo.com>
2015-04-20 17:54:23 +0000

    update

--- bibliography_management.php
+++ bibliography_management.php
@@ -4,6 +4,45 @@
 		<title>Manage your bibliographies</title>
 	</head>
 	<body>
+		
+		<?php
+		$uName=$_POST['name'];
+		$pWord=$_POST['password'];
+		
+		 $conn = oci_connect('username', 'password', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+        $query = 'SELECT users.userID, users.password FROM users WHERE users.userID='$uName' AND users.password='$pWord'';
+        
+        $stid = oci_parse($conn,$query);
+        oci_execute($stid,OCI_DEFAULT);
+        
+        
+        $check=0;
+        while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+        {
+           foreach ($row as $item) 
+           {
+              $check=$check+1;
+           }
+        }
+        oci_free_statement($stid);
+        oci_close($conn); 
+        
+        session_destroy();
+        session_start();
+        
+        if($check==1)
+        {
+        	$_SESSION['name']=$uName;
+        }
+        else
+        {
+        	$_SESSION['errorMessage']="invalid username or password";
+        	header("Location: login.php");
+        	exit();
+        }
+		
+		?>
 
 		<?php require 'header.php' ?>
 
--- login.php
+++ login.php
@@ -10,44 +10,12 @@ $errorMessage="";
 	<header><h1>Login</h1></header>
 	<body>
 		<span class="error">* <?php echo $errorMessage;?></span><br>
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+		<form action="bibliography_managment.php" method="post">
 		Name: <br><input type="text" name="name"><br>
 		Password: <br><input type="text" name="password"><br><br>
 		<input id="button" type="submit" name="submit" value="Log-In">
 		</form><br>
 		
 		<a href="register.php">Register</a>
-		<?php
-			session_start();
-			
-            $conn = oci_connect('username', 'password', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-
-            $query = 'SELECT users.userID, users.password FROM users WHERE users.userID='$uName' AND users.password='$pWord'';
-            
-            $stid = oci_parse($conn,$query);
-            oci_execute($stid,OCI_DEFAULT);
-            
-            
-            $check=0;
-            while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
-            {
-               foreach ($row as $item) 
-               {
-                  $check=$check+1;
-               }
-            }
-            oci_free_statement($stid);
-            oci_close($conn); 
-            
-            if(check==1)
-            {
-                $_SESSION['name']=$uName;
-                $errorMessage="";
-            }
-            else
-            {
-            	$errorMessage="invalid username or password";			
-            }
-		?>
 	</body>
 </html>
