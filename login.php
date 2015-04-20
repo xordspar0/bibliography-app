368af442f39748fe4f1da8bf37c0a22f4ae19409
JoshMindermann <joshmindermann@yahoo.com>
2015-04-20 15:21:52 -0500

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ $pWord="";
 	<body>
 		<?php echo $_SESSION['errorMessage']; ?><br>
 		<span class="error">* <?php echo $errorMessage;?></span><br>
-		<form action="bibliography_managment.php" method="post">
+		<form action="bibliography_management.php" method="post">
 		Name: <br><input type="text" name="name"><br>
 		Password: <br><input type="text" name="password"><br><br>
 		<input id="button" type="submit" name="submit" value="Log-In">
