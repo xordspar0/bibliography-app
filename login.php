ef747f4e3e3bc02f0387c1653a0ec7449ddedfe7
JoshMindermann <joshmindermann@yahoo.com>
2015-04-21 13:21:08 -0500

    Update login.php

--- login.php
+++ login.php
@@ -14,13 +14,14 @@ $pWord="";
 	</head>
 	<header><h1>Login</h1></header>
 	<body>
-		<?php echo $_SESSION['errorMessage']; ?><br>
+		<br>
 		<span class="error">* <?php echo $errorMessage;?></span><br>
 		<form action="bibliography_management.php" method="post">
 		Name: <br><input type="text" name="name"><br>
 		Password: <br><input type="text" name="password"><br><br>
 		<input id="button" type="submit" name="submit" value="Log-In">
 		</form><br>
+		<?php echo $_SESSION['errorMessage']; ?><br>
 		<a href="register.php">Register</a>
 	</body>
 </html>
