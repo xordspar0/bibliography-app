d92638857077009865e2f9cf059834e5d687828a
Jordan Christiansen <xordspar0@gmail.com>
2015-04-22 17:07:45 +0000

    Straightened out the login checking code

--- check_login.php
+++ check_login.php
@@ -4,31 +4,29 @@ require "dbconnect.php";
 
 $uName=$pWord=$errorMsg="";
                 
-session_start();
 $uName=$_POST["name"];
 $pWord=$_POST["pWord"];
 
 $conn = dbconnect();
 
+// TODO: do sanitation to prevent SQL injections
 $query = 'SELECT users.userID, users.password FROM users WHERE users.userID=' . $uName . ' AND users.password=' . $pWord . '';
 
 $stid = oci_parse($conn,$query);
 oci_execute($stid,OCI_DEFAULT);
         
-$check=0;
-while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+if (oci_fetch_array($stid,OCI_ASSOC))
 {
-        foreach ($row as $item) 
-        {
-                $check=$check+1;
-        }
-}
-oci_free_statement($stid);
-oci_close($conn); 
-
-if(check==1)
-{
+        session_start();
         $_SESSION['name']=$uName;
+        // Redirect to the bibliography management page
 }
+else
+{
+        // Return to the login page
+}
+
+oci_free_statement($stid);
+oci_close($conn);
 
 ?>
