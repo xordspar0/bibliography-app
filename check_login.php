3f82704fab761df9981522c9743bc0b04efe45aa
Jordan Christiansen <xordspar0@gmail.com>
2015-04-22 16:58:01 +0000

    Corrected formatting and syntax errors in check_login.php

--- check_login.php
+++ check_login.php
@@ -1,37 +1,34 @@
-<!DOCTYPEHTML>
+<?php
 
-        <?php
-                require "dbconnect.php";
+require "dbconnect.php";
 
-                $uName=$pWord=$errorMsg="";
+$uName=$pWord=$errorMsg="";
                 
-                session_start();
-                $uName=$_POST["name"];
-                $pWord=$_POST["pWord"];
-                
-                $conn = dbconnect();
-                
-                //put your query in here
-                $query = 'SELECT users.userID, users.password FROM users WHERE users.userID='$uName' AND users.password='$pWord'';
-                
-                $stid = oci_parse($conn,$query);
-                oci_execute($stid,OCI_DEFAULT);
-                
-                
-                $check=0;
-                while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
-                {
-                   foreach ($row as $item) 
-                   {
-                      $check=$check+1;
-                   }
-                }
-                oci_free_statement($stid);
-                oci_close($conn); 
-                
-                if(check==1)
-                {
-                        $_SESSION['name']=$uName;
-                }
-        ?>
-    
+session_start();
+$uName=$_POST["name"];
+$pWord=$_POST["pWord"];
+
+$conn = dbconnect();
+
+$query = 'SELECT users.userID, users.password FROM users WHERE users.userID=' . $uName . ' AND users.password=' . $pWord . '';
+
+$stid = oci_parse($conn,$query);
+oci_execute($stid,OCI_DEFAULT);
+        
+$check=0;
+while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+{
+        foreach ($row as $item) 
+        {
+                $check=$check+1;
+        }
+}
+oci_free_statement($stid);
+oci_close($conn); 
+
+if(check==1)
+{
+        $_SESSION['name']=$uName;
+}
+
+?>
