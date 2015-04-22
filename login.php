5af796d8c3cd46c6ac9e90df9872592f4784a525
Jordan Christiansen <xordspar0@gmail.com>
2015-04-22 17:14:09 +0000

    Switched the password field to an actual html password form field

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ $pWord="";
 		<span class="error">* <?php echo $errorMessage;?></span><br>
 		<form action="bibliography_management.php" method="post">
 		Name: <br><input type="text" name="name"><br>
-		Password: <br><input type="text" name="password"><br><br>
+		Password: <br><input type="password" name="password"><br><br>
 		<input id="button" type="submit" name="submit" value="Log-In">
 		</form><br>
 		<?php echo $_SESSION['errorMessage']; ?><br>
