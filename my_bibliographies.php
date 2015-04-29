e836f234d74ae83e128f11d5c9713052ea30425e
joshmindermann <joshmindermann@yahoo.com>
2015-04-29 20:32:42 +0000

    fiddled

--- my_bibliographies.php
+++ my_bibliographies.php
@@ -13,26 +13,27 @@
     FROM bibliographies, users 
     WHERE users.userID = (ID of the current user) AND users.userID = bibliographies.userID"-->
     <?php
+		require('config.php');
         session_start();
         $currentUser=$_SESSION['name'];
         $conn = oci_connect($dbuser, $dbpass, $dbconn);
     				
-    		        $query = "SELECT bibliographies.name 
-                              FROM bibliographies, users 
-                              WHERE users.userID = '$currentUser' AND users.userID = bibliographies.userID";
+    		        $query = "SELECT *  
+                              FROM bibliographies 
+                              WHERE userID = :currentUser";
     		        
-    		        $stid = oci_parse($conn,$query);
-    		        oci_execute($stid,OCI_DEFAULT);
+	        $stid = oci_parse($conn,$query);
+			oci_bind_by_name($stid, ":currentUser", $currentUser);
+        	oci_execute($stid,OCI_DEFAULT);
     ?>
        <!--These are test widths to be changed later-->
-    <table width="900" border="1">  
+    <table width="600" border="1">  
         <tr>  
-        <th width="91"> <div align="center">BibliographyID </div></th>  
-        <th width="59"> <div align="center">Name </div></th>  
-        <th width="98"> <div align="center">OwnerID </div></th>  
-        <th width="120"> <div align="center">Last Modified </div></th>  
-        <th width="120"> <div align="center">Date Created </div></th>  
-        <th width="30"> <div align="center">Delete</div></th>
+        <th width="100"> <div align="center">BibliographyID </div></th>  
+        <th width="70"> <div align="center">Name </div></th>  
+        <th width="70"> <div align="center">OwnerID </div></th>  
+        <th width="100"> <div align="center">Last Modified </div></th>  
+        <th width="100"> <div align="center">Date Created </div></th>  
         </tr>  
     <?php   
         while($objResult = oci_fetch_array($stid,OCI_BOTH))  
@@ -44,11 +45,6 @@
                 <td> <div align="center"><?php echo $objResult['users.userID'];?></div></td>
                 <td> <div align="center"><?php echo $objResult['dateModified'];?></div></td>
                 <td> <div align="center"><?php echo $objResult['dateCreated'];?></div></td>
-                <td> <div align="center"><a href="delete.php?bID=<?php=$objResult['bID'];?>">Delete</a></div></td>
-                <!--Confirm Delete
-                <td align="center"><a href="JavaScript:if(confirm('Confirm Delete?')==true)
-                {window.location='delete.php?bID=<?php=$objResult['bID'];?>';}">Delete</a></td>
-                -->
             </tr>
     <?php        
         }
