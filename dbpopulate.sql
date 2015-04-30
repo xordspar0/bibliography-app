b418be2874f389ba969fd400b7d4d4c672185c64
JoshMindermann <joshmindermann@yahoo.com>
2015-04-30 10:55:11 -0500

    Create dbpopulate.sql

---
+++ dbpopulate.sql
@@ -0,0 +1,3 @@
+INSERT INTO bibliographies VALUES(1,'testID','01-JUN-1995', '01-JUN-1995', 'testBib');
+INSERT INTO citations VALUES(1, 1, 'Bob', 'Ross', 'Joy of Painting', 'book');
+INSERT INTO books VALUES(1, 'Fargo', 'Publishman', 1998);
