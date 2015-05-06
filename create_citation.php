5a33df98a6d1489fa68f0b03c9dea15795a02537
joshmindermann <joshmindermann@yahoo.com>
2015-05-06 17:17:27 +0000

    Filled query variables

--- create_citation.php
+++ create_citation.php
@@ -1,5 +1,6 @@
 <?php
 	$postVars = $_POST;
+	$currentBib = $_GET['bID'];
 	require "require_login.php";
 	require "config.php";
 	
@@ -13,10 +14,18 @@
 		
 		$query="INSERT INTO citations
 				VALUES(seq_citation.nextval, 
-					   //TODO: insert appropriate variables
-					   ";
+					   :bID,
+					   :first,
+					   :last,
+					   :title,
+					   :medium)";
 		
     	$stid = oci_parse($conn, $query);
+    	oci_bind_by_name($stid, ":bID", $currentBib);
+    	oci_bind_by_name($stid, ":first", $_POST["first"]);
+    	oci_bind_by_name($stid, ":last", $_POST["last"]);
+    	oci_bind_by_name($stid, ":title", $_POST["title"]);
+    	oci_bind_by_name($stid, ":medium", $_GET["type"]);
         oci_execute($stid,OCI_DEFAULT);
 		oci_commit($conn);
         oci_free_statement($stid);
@@ -24,29 +33,36 @@
 		if ($_GET["type"] == "book")
 		{
     		$query="INSERT INTO books
-    				VALUES(seq_citation.nextval, 
-    					   //TODO: insert appropriate variables
-    					   ";
+    				VALUES(seq_citation.currval, 
+    					   :city,
+    					   :publisher,
+    					   :yearPublished)";
     		$stid = oci_parse($conn, $query);
-            //TODO: bind the variables the the query
+            oci_bind_by_name($stid, ":city", $_POST["city"]);
+            oci_bind_by_name($stid, ":publisher", $_POST["publisher"]);
+            oci_bind_by_name($stid, ":yearPublished", $_POST["yearPublished"]);
 		}
 		elseif ($_GET["type"] == "periodical")
 		{
 		    $query="INSERT INTO periodicals
-    				VALUES(seq_citation.nextval, 
-    					   //TODO: insert appropriate variables
-    					   ";
+    				VALUES(seq_citation.currval, 
+    					   :name,
+    					   :pubDate,
+    					   :pageNum)";
     		$stid = oci_parse($conn, $query);
-            //TODO: bind the variables the the query
+            oci_bind_by_name($stid, ":name", $_POST["name"]);
+            oci_bind_by_name($stid, ":pubDate", $_POST["pubDate"]);
+            oci_bind_by_name($stid, ":pageNum", $_POST["pageNum"]);
 		}
 		elseif ($_GET["type"] == "web")
 		{
 		    $query="INSERT INTO website
-    				VALUES(seq_citation.nextval, 
-    					   //TODO: insert appropriate variables
-    					   ";
+    				VALUES(seq_citation.currval, 
+    					   :name,
+    					   :pubDate)";
     		$stid = oci_parse($conn, $query);
-            //TODO: bind the variables the the query
+            oci_bind_by_name($stid, ":name", $_POST["name"]);
+            oci_bind_by_name($stid, ":pubDate", $_POST["pubDate"]);
 		}
 		
 		oci_execute($stid,OCI_DEFAULT);
